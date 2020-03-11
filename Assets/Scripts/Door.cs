7e15718839e68a222c0c6c080cc0f0b1ba0a2620
Paultje <44833263+GoggledPetilil@users.noreply.github.com>
2020-03-11 17:00:52 +0100

    Room expansion

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -7,12 +7,14 @@ public class Door : MonoBehaviour, IInteractable
 
     public int id;
     public bool open;
+    private float initialPosition;
     private float initialRotation;
 
     // Start is called before the first frame update
     void Start()
     {
 
+        initialPosition = transform.position.y;
         initialRotation = transform.rotation.eulerAngles.y;
 
     }
@@ -20,7 +22,22 @@ public class Door : MonoBehaviour, IInteractable
     // Update is called once per frame
     void Update()
     {
+        if (open && transform.position.y > initialPosition - (transform.localScale.y * 0.95f))
+        {
+            transform.position = Vector3.MoveTowards(
+                current: transform.position, 
+                target: new Vector3(transform.position.x, initialPosition - (transform.localScale.y * 0.95f), transform.position.z), 
+                maxDistanceDelta: 0.2f);
+        }
+        else if (!open && transform.position.y < initialPosition)
+        {
+            transform.position = Vector3.MoveTowards(
+                current: transform.position,
+                target: new Vector3(transform.position.x, initialPosition, transform.position.z),
+                maxDistanceDelta: 0.2f);
+        }
 
+        /*
         if (open && transform.rotation.eulerAngles.y < initialRotation + 80)
         {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, initialRotation + 80, 0), 5);
@@ -29,6 +46,7 @@ public class Door : MonoBehaviour, IInteractable
         {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, initialRotation, 0), 5);
         }
+        */
 
     }
 
